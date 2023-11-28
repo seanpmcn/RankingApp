a8e7f48387bee058196fd7e4dd50d9d14cc55e16
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2023-11-27 23:01:01 -0500

    Add RankItem

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -14,6 +14,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  },
+  {
+    path: '/rank-items',
+    element: <RankItems />
   }
 ];
 
---
+++ ClientApp/src/components/RankItems.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+
+const RankItems = () => {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, [])
+
+    return  (
+        <main>
+            {
+                (items != null)?items.map((item) => <h3>{item.title }</h3>): <div>Loading...</div>
+            }
+
+        </main>
+    )
+
+}
+export default RankItems;
