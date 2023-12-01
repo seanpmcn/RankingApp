1f4ebd1fd321467545c174e3767a3eca8137361c
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2023-11-30 22:06:11 -0500

    Add movie images, model loading

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import MovieImageArr from './MovieImages';
 
 const RankItems = () => {
 
@@ -17,10 +18,13 @@ const RankItems = () => {
 
     return  (
         <main>
+            <div className="items-not-ranked">
             {
-                (items != null)?items.map((item) => <h3>{item.title }</h3>): <div>Loading...</div>
+                (items.length > 0) ? items.map((item) => 
+                   <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id === item.imageId)?.image} alt={item.title}/>
+                    ) : <div>Loading...</div>
             }
-
+            </div>
         </main>
     )
 
--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -3,7 +3,7 @@ namespace RankingApp.Models
     public class ItemModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public int ImageId { get; set; }
         public int Ranking { get; set; }
         public int ItemType { get; set; }
