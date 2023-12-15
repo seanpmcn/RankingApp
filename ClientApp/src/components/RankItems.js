4160a59d4c1bd30f512dc7067f0715e26be7706f
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2023-12-14 21:05:03 -0500

    Add drag and drop functionality

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -7,6 +7,29 @@ const RankItems = () => {
     const [items, setItems] = useState([]);
     const dataType = 1;
 
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElem = ev.target;
+        if(targetElem.nodeName === "IMG") {
+            return false;
+        }
+        if(targetElem.childNodes.length === 0) {
+            console.log(ev.dataTransfer.getData("text"));
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                {...item, ranking: parseInt(targetElem.id.substring(5))} : {...item, ranking: item.ranking});
+            setItems(transformedCollection);
+        }
+    }
+
     useEffect(() => {
         fetch(`item/${dataType}`)
             .then((results) => {
@@ -19,12 +42,14 @@ const RankItems = () => {
 
     return  (
         <main>
-            <RankingGrid items={items} imgArr={MovieImageArr}/>
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop}/>
             <div className="items-not-ranked">
             {
                 (items.length > 0) ? items.map((item) =>
                     <div className="unranked-cell"> 
-                        <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id === item.imageId)?.image} alt={item.title}/>
+                        <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}
+                            style={{cursor:"pointer"}} draggable="true" onDragStart={drag}
+                        />
                     </div>
                     ) : <div>Loading...</div>
             }
